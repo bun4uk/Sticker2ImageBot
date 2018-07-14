698c56c3a5c2638040b0f6066f9231d529dfcdd6
V.Bunchuk <v.bunchuk@iotechnologies.com>
2018-07-14 18:57:18 +0300

    cron configure

--- st2img_usage_cron.php
+++ st2img_usage_cron.php
@@ -11,16 +11,13 @@ include 'Dictionary.php';
 include 'TelegramBot.php';
 
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
-$token = trim(file_get_contents('./config/sticker2img'));
+$token = trim(file_get_contents(__DIR__ . '/config/sticker2img'));
 $log = new Logger('img_log');
 $telegramApi = new TelegramBot($token, $log);
 
-
-$date = (new \DateTime())->format('Y-m-d');
-
-exec("cat logs/img_log.log | grep === | grep {$date} | wc -l", $result);
+$date = (new \DateTime('yesterday'))->format('Y-m-d');
+exec('cat ' . __DIR__ . "/logs/img_log.log | grep === | grep {$date} | wc -l", $result);
 $telegramApi->sendMessage(7699150, reset($result));
 return true;
 
